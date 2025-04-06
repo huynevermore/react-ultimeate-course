f70970bba94ade55e35c286b3b4fd80e35487c8c
Tony Nguyen <tonynguyen@192.168.0.154>
2025-04-06 23:01:24 +0700

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { Link, Routes, Route } from 'react-router-dom';
 import JSX from './JSX';
 import Props from './Props';
 import Button from './components/Button';
@@ -30,25 +31,74 @@ function App() {
 
   return (
     <>
-      <JSX />
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link 
+                  to="/jsx"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                >
+                  JSX
+                </Link>
+              </li>
+              <li>
+                <Link 
+                  to="/props"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                >
+                  Props
+                </Link>
+              </li>
+              <li>
+                <Link 
+                  to="/state"
+                  replace
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                >
+                  State
+                </Link>
+              </li>
+              <li>
+                <Link 
+                  to="/handle-event"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                >
+                  Handle Event
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-      <Props 
-        {...address}
-        city="HN"
-        firstName="tony" // string
-        isMale={true} // boolean
-        age={18} // number
-        colors={['red', 'blue']} // array
-        onSubmit={() => {}} // function
-        first-name="tony multiword"
-        component1={Button}
-        component2={<Button type='button' />}
-      />
+      <main>
+        <Routes>
+          <Route path="/jsx" element={<JSX />} />
+          <Route 
+            path="/props" 
+            element={
+              <Props 
+                {...address}
+                city="HN"
+                firstName="tony" // string
+                isMale={true} // boolean
+                age={18} // number
+                colors={['red', 'blue']} // array
+                onSubmit={() => {}} // function
+                first-name="tony multiword"
+                component1={Button}
+                component2={<Button type='button' />}
+              />
+            } 
+          />
+          <Route path="/state" element={<State />} />
+          <Route path="/handle-event" element={  <HandleEvent />} />
+        </Routes>
+      </main>
 
-      <State />
-
-      <br />
-      <HandleEvent />
+      <hr />
 
       <br />
       <Condition />
--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import App from './App.tsx'
 import { AppProvider } from './context/AppContext.tsx';
 import { Provider } from 'react-redux';
 import { store } from './store.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-  <Provider store={store}>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </Provider>
+  </BrowserRouter>
   // </StrictMode>,
 )
